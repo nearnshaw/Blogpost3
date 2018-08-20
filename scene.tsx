91a486700c6e4a7f986c242c6cbddc1ded93d08e
Nico E <earnshaw.nico@gmail.com>
2018-08-20 13:08:21 -0300

    restructured

--- scene.tsx
+++ scene.tsx
@@ -2,7 +2,8 @@ import * as DCL from 'metaverse-api'
 import { Vector3Component } from 'metaverse-api';
 const axios = require('axios');
 
-const fakeWeather: string = "storm thunder"
+let fakeWeather: string | null = "light rain"
+
 
 const appId : string = "bb6063b3"
 const APIkey : string = "2e55a43d3e62d76f145f28aa7e3990e9"
@@ -11,9 +12,9 @@ const lon: string = "-58.46"
 
 
 let objectCounter: number = 0
-//let lightningNum: number = 0
 const dropSpeed: number = 3000
 const flakeSpeed: number = dropSpeed*4
+let precipitationLoop: any = null
 
 const callUrl : string = "http://api.weatherunlocked.com/api/current/" + lat + ",%20" + lon + "?app_id=" + appId + "&app_key=" + APIkey
 
@@ -52,18 +53,27 @@ export default class HouseScene extends DCL.ScriptableScene<any, IState> {
   sceneDidMount() {
     setInterval(
       this.getWeather()
-    , 1000000)
+    , 10000)
   }
 
   getWeather() { 
-      axios.get(callUrl)
+    console.log("getting new weather")
+    axios.get(callUrl)
       .then( (response:any) => {
         console.log(response.data.wx_desc)
+        let weather: Weather
         if (fakeWeather){
-          this.mapWeather(fakeWeather)}
-        else{
-          this.mapWeather(response.data.wx_desc)
+          weather = this.mapWeather(fakeWeather)
+          fakeWeather = null
         }
+        else{
+          weather = this.mapWeather(response.data.wx_desc)
+        }
+        if (weather == this.state.weather) {return}
+        clearInterval(precipitationLoop)
+        this.setState({weather: weather })
+        if (weather == (Weather.sun | Weather.clouds)) {return}
+        this.startPrecipitation()
  
       })
       .catch( (error:any) => {
@@ -72,34 +82,51 @@ export default class HouseScene extends DCL.ScriptableScene<any, IState> {
   }
 
   mapWeather(weather: string){
-  
     let simpleWeather: Weather
     if ( weather.match(/(thunder)/gi) ){       //// can we do this neater w/out so many ifs?
       simpleWeather = Weather.storm;
-      this.startRain(100)
     } else if (weather.match(/(snow|ice)/gi) ){
       simpleWeather = Weather.snow
-      this.startFlakes(500)
     } else if (weather.match(/(heavy|torrential)/gi) ){
-      simpleWeather = Weather.heavyRain
-      this.startRain(50)
+      simpleWeather = Weather.heavyRain 
     } else if (weather.match(/(rain|drizzle|shower)/gi) ){
       simpleWeather = Weather.rain
-      this.startRain(500)
     } else if (weather.match(/(cloud|overcast|fog|mist)/gi) ){
       simpleWeather = Weather.clouds
     } else {
       simpleWeather = Weather.sun
     }
-    this.setState({weather: simpleWeather })
-  
-    console.log(weather)
-    console.log(this.state.weather)  
+    console.log("literal weather: " + weather)
+    console.log("simple weather: " + simpleWeather)  
+    return simpleWeather
+  }
+
+  startPrecipitation(){
+    switch(this.state.weather){
+      case Weather.storm:
+        this.startRain(100)
+        break
+      case Weather.snow:
+        this.startSnow(500)
+        break 
+      case Weather.heavyRain:
+        this.startRain(50)
+        break
+      case Weather.rain:
+        this.startRain(500)
+        break
+    }
   }
 
   startRain(interval: number){
-    setInterval( f =>{
+    precipitationLoop = setInterval( f =>{
       this.addDrop()
+      }, interval)
+  }
+
+  startSnow(interval: number){
+    precipitationLoop = setInterval( f =>{
+      this.addFlake()
       }, interval)  
   }
 
@@ -122,15 +149,8 @@ export default class HouseScene extends DCL.ScriptableScene<any, IState> {
       dropsAdded = Object.create(this.state.drops)
       delete dropsAdded.dropName    
       this.setState({drops: dropsAdded })
-      console.log("deleted" + dropName )
+      //console.log("deleted" + dropName )
       }, dropSpeed)
-
-  }
-  
-  startFlakes(interval: number){
-    setInterval( f =>{
-      this.addFlake()
-      }, interval)  
   }
 
   async addFlake(){
@@ -162,7 +182,7 @@ export default class HouseScene extends DCL.ScriptableScene<any, IState> {
       flakesAdded = Object.create(this.state.flakes)
       delete flakesAdded.flakeName    
       this.setState({flakes: flakesAdded })
-      console.log("deleted" + flakeName )
+      //console.log("deleted" + flakeName )
       }, flakeSpeed)
 
   }
@@ -266,13 +286,8 @@ export default class HouseScene extends DCL.ScriptableScene<any, IState> {
   }
 
   renderLightNing(){
-    let lightningNum:number = Math.floor(Math.random()*25)
-    //lightningNum =+  Math.floor( (Math.random()*2 ) -1)
-    //lightningNum % 5
-    if (lightningNum>6)
-    {
-      return
-    }
+    let lightningNum:number = Math.floor(Math.random()*25) + 1
+    if (lightningNum>6) {return}
     return (
       <gltf-model 
         src = {"models/ln" + lightningNum + ".gltf"}
